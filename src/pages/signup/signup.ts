0278302d02c3adb5d8a9b066f6d1c22cd86d895b
Marchenay Anna <anna.marchenay@etu.udamail.fr>
2018-06-12 17:41:20 +0200

    changements du formulaire

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseProvider } from './../../providers/firebase/firebase';
 import { AngularFireList } from 'angularfire2/database';
 import {User} from "../../model/user";
+import {FormBuilder, FormControlName, FormGroup, Validators} from "@angular/forms";
 
 /**
  * Generated class for the SignupPage page.
@@ -23,24 +24,38 @@ export class SignupPage {
   mail : String;
   mdp : String;
   ddn : Date;
+  private formulaire : FormGroup;
 
 
   userItems: AngularFireList<{}>;
   newItem = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,  public firebaseProvider: FirebaseProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,  public firebaseProvider: FirebaseProvider, private formBuilder : FormBuilder ) {
 
     this.today = new Date();
     this.userItems = this.firebaseProvider.getUsersItems();
+    this.formulaire = this.formBuilder.group({
+      sName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      fName: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      mail: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'), Validators.required])],
+      psw: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      bDate: ['', Validators.compose([ Validators.required])]
+
+    });
   }
 
   addUserItem() {
-    this.firebaseProvider.addUserItem(new User(this.mail, this.mdp, this.prenom, this.nom, this.ddn));
+    this.firebaseProvider.addUserItem(new User(this.formulaire.value['mail'],this.formulaire.value['psw'], this.formulaire.value['fName'], this.formulaire.value['sName'], this.formulaire.value['bDate']));
+
   }
 
   removeUserItem(id) {
     this.firebaseProvider.removeUserItem(id);
   }
 
+  signupForm(){
+      this.addUserItem();
+
+  }
 
 }
